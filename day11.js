8258c7c4707f265b646f0042bc88f56c808df3c5
rubnvp <rubn.vp@gmail.com>
2024-12-12 09:57:03 +0100

    day11 part2

--- day11.js
+++ day11.js
@@ -32,18 +32,63 @@ function solve1(stones) {
   return stones.length;
 }
 
+// Mini branches approach with memoization
 function solve2(stones) {
-  return stones;
+  // const BLINKS = 75;
+  for (let blink = 0; blink < 37; blink++) {
+    console.log('Blink:', blink, 'Stones:', stones.length);
+    stones = stones.reduce((stones, num) => {
+      const stoneString = num.toString();
+      if (num === 0) {
+        stones.push(1);
+      } else if (stoneString.length % 2 === 0) {
+        stones.push(Number(stoneString.slice(0, stoneString.length / 2)));
+        stones.push(Number(stoneString.slice(stoneString.length / 2)));
+      } else {
+        stones.push(num * 2024);
+      }
+      return stones;
+    }, []);
+  }
+  const memo ={};
+  let totalCount = 0;
+  const totalLengthApprox = Math.round(stones.length / 10);
+  for (let i = 0; i < stones.length; i++) {
+    const seedStone = stones[i];
+    let miniBranch = [seedStone];
+    if (!memo[seedStone]) {
+      for (let blink = 37; blink < 75; blink++) {
+        miniBranch = miniBranch.reduce((stones, num) => {
+          const stoneString = num.toString();
+          if (num === 0) {
+            stones.push(1);
+          } else if (stoneString.length % 2 === 0) {
+            stones.push(Number(stoneString.slice(0, stoneString.length / 2)));
+            stones.push(Number(stoneString.slice(stoneString.length / 2)));
+          } else {
+            stones.push(num * 2024);
+          }
+          return stones;
+        }, []);
+      }
+      memo[seedStone] = miniBranch.length;
+    }
+    totalCount += memo[seedStone];
+    if (i % 10 === 0) {
+      console.log('Mini branches progress:', i / 10, 'of :', totalLengthApprox);
+    }
+  }
+  return totalCount;
 }
 
 const input = parseInput(inputText);
-// console.log('Solution 1:', solve1(input));
-console.log('Solution 2:', solve2(input));
+console.log('Solution 1:', solve1(input));
+console.log('Solution 2:', solve2(input)); // currently takes 15 min
 console.timeEnd('✨ Done in');
 
 // Failed solution2 attempts
 
-// Nested array approach
+// Nested array approach (failed because out of memory)
 // function solve2(stones) {
 //   stones = stones.slice();
 //   const BLINKS = 75;
@@ -83,7 +128,7 @@ console.timeEnd('✨ Done in');
 // }
 
 
-// Single string approach
+// Single string approach (failed because out of memory)
 // function solve2(stones) {
 //   const BLINKS = 75;
 //   stones = stones.join(' ');
